1706ce174390d9eb35aaa0643ed3ae4575959293
progress0407 <progress0407@gmail.com>
2023-09-24 23:51:49 +0900

    초기 프로젝트 생성

---
+++ src/main/java/philo/jvmlabtools27/x/Application.java
@@ -0,0 +1,13 @@
+package philo.jvmlabtools27.x;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/philo/jvmlabtools27/x/ApplicationTests.java
@@ -0,0 +1,13 @@
+package philo.jvmlabtools27.x;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
